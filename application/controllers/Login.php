e1dc46b6bc7c00dd0d0b334b4da43d3e0f8335f5
Christian <cpm.110996@gmail.com>
2017-05-10 19:04:23 +0200

    chistian

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -1,43 +1,44 @@
 <?php     
-    defined('BASEPATH') OR exit('No direct script access allowed');
-    
-    class Login extends CI_Controller {
+defined('BASEPATH') OR exit('No direct script access allowed');
 
-        function __construct(){
-            parent::__construct();
-        }
+class Login extends CI_Controller {
 
-        function index(){            
-                $data['main_content'] = 'index_View'; 
-                $this->parser->parse('includes/template',$data);
-        }
-
-        public function loguearse(){
-            $data = $this->input->post();
-            $this->load->model("User");
-            $correctPass = $this->User->get_USR_pass_where($data);
-            if ($correctPass) {
-                $usuarioRegistrado = $this->User->get_USR_all_where($data['nickL']);
-                $this->crearSesion($usuarioRegistrado);
-                redirect('/');
-                }
-            }
-
-            function crearSesion($usuarioRegistrado){
-                $newSesion = array(
-                    'nick' => $usuarioRegistrado[0]['USR_nick'],
-                    'email' => $usuarioRegistrado[0]['USR_email'],
-                    'permiso' => $usuarioRegistrado[0]['USR_permiso']
-                    );
-                $this->session->($newSesion);
-            }
-
-            function cerrarSesion(){
-                /*$this->session->sess_destroy();*/
-                /*$this->session->unset_userdata($this->session);*/
-                print_r($newSesion);
-                die;
-                redirect('/');
-            }
+    function __construct(){
+        parent::__construct();
     }
+
+    function index(){            
+        $data['main_content'] = 'index_View'; 
+        $this->parser->parse('includes/template',$data);
+    }
+
+    public function loguearse(){
+        $data = $this->input->post();
+        $this->load->model("User");
+        $correctPass = $this->User->get_USR_pass_where($data);
+        if ($correctPass) {
+            $usuarioRegistrado = $this->User->get_USR_all_where($data['nickL']);
+            $this->crearSesion($usuarioRegistrado);
+            redirect('/');
+        }
+    }
+
+    function crearSesion($usuarioRegistrado){
+        $userdata = array(
+         'nick' => $usuarioRegistrado[0]['USR_nick'],
+         'email' => $usuarioRegistrado[0]['USR_email'],
+         'permiso' => $usuarioRegistrado[0]['USR_permiso'],
+         'logueado' => TRUE
+         );
+        $this->session->set_userdata($userdata);  
+    }
+
+    function cerrarSesion(){
+      $userdata = array(
+         'logueado' => FALSE
+         );
+      $this->session->sess_destroy();
+      redirect('/');
+  }
+}
 ?>
