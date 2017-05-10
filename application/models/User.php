c828c06493b7b4c1a1777861b5dd31bf748562dc
kiwips <kaydan10@gmail.com>
2017-05-10 17:02:02 +0200

    fix User model

--- application/models/User.php
+++ application/models/User.php
@@ -53,7 +53,7 @@ class user extends CI_Model{
             $this->db->select('USR_nick, USR_email, USR_permiso');
             $query = $this->db->get_where('user',array("USR_nick"=>$nick));
             return $query->result_array();
-
+        }
         public function get_USR_nick_permiso(){
             $this->db->select('USR_nick,USR_permiso');
             $query=array(
