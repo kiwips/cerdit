090af9a1e8d2100f4bb66123cf26f9c670f63a60
kiwips <kaydan10@gmail.com>
2017-05-10 20:30:58 +0200

    a

---
+++ application/views/index_View.php
@@ -0,0 +1,37 @@
+<div class="jumbotron">
+	<div class="container text-center">	
+		<h1 class="titulo">DreamPC</h1>
+	</div>
+</div>
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+      <a class="navbar-brand" href="#">Logo</a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Home</a></li>
+        <li><a href="#">Products</a></li>
+        <li><a href="#">Deals</a></li>
+        <li><a href="#">Stores</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+      
+      <?php if ($this->session->userdata('logueado')) { ?>
+          <li><a href="permiso"><span class="glyphicon glyphicon-cog"></span> Administrar </a></li>
+          <li><a href="finSesion"><span class="glyphicon glyphicon-log-out"></span> Finalizar Sesión </a></li>
+        <?php } else{ ?>
+              <li><a data-toggle="modal" data-target="#login"><span class="glyphicon glyphicon-user"></span> Login</a></li>
+            <?php require ('modalL.php'); ?>
+        <?php } ?>
+        <li><a href="#"><span class="glyphicon glyphicon-shopping-cart"></span> Cart</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
